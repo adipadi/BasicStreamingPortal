e92f986bbec07b0f2311a9851f9763fe39b40892
Sondre T. Johannessen <sondretjohannessen@Sondres-MBP.vimond.local>
2016-10-11 15:23:15 +0200

    oppgave_1_c

--- project/src/client/cinemaReducer.js
+++ project/src/client/cinemaReducer.js
@@ -22,18 +22,7 @@ export default function featureReducer(state = initialState, action) {
       };
     }
 
-    case CinemaConstants.GET_ASSETS_SUCCESS: {
-      const res = action.result && action.result.data && action.result.data.assets;
-      const assets = res.asset;
-      const numberOfHits = res.numberOfHits;
-      return {
-        ...state,
-        assets,
-        numberOfHits,
-        assetsLoading: false,
-        showInitialHeader: true
-      };
-    }
+  // Make a case for get assets success
 
     case CinemaConstants.GET_ASSETS_FAILURE: {
       return initialState;
